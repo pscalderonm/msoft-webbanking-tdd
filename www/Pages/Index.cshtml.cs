81110296a6cef3619e724cb7dd63f884d8f9dcfa
Pablo S. Calderon <pscalderonm@gmail.com>
2023-01-07 14:10:24 +0000

    Complete web interface Fix gitignore files

--- www/Pages/Index.cshtml.cs
+++ www/Pages/Index.cshtml.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using bankingLibrary;
 
 namespace www.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+
+    private static readonly Operation BankSystem = new Operation(); 
+
+    public static BankAccount? Account { get; set; }
+
+    [BindProperty]
+    public string operation { get; set; }
+
+    [BindProperty]
+    public decimal amount { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
@@ -14,6 +25,25 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+      
+    }
 
+    public void OnPostOpenAccount()
+    {
+      Account = BankSystem.OpenBankAccount();
+    }
+
+    public void OnPostExecuteOperation()
+    {
+      switch(operation)
+      {
+        case "D":
+          Account = BankSystem.DoDeposit(Account.Code, amount);
+          break;
+        case "R":
+          Account = BankSystem.DoWithdraw(Account.Code, amount);
+          break;
+      }
+      amount = 0;
     }
 }
